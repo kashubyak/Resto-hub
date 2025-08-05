187534ed9bb55cad7c90a90e885e5268845fd1a0
kashubyak Mykola <kashubyak514@gmail.com>
2025-08-05 23:23:42 +0300

    feat: add LoginCompany component with AuthLayout for login functionality

---
+++ frontend/src/app/(auth)/LoginCompany.tsx
@@ -0,0 +1,9 @@
+import { AuthLayout } from './AuthLayout'
+
+export const LoginCompany = () => {
+	return (
+		<AuthLayout title='Login' onSubmit={() => {}}>
+			<div className=''>login</div>
+		</AuthLayout>
+	)
+}
