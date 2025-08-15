e878966641508a9c25d1cb36dd76cbefe4cfaf4e
kashubyak Mykola <kashubyak514@gmail.com>
2025-08-15 15:00:25 +0300

    feat: update user controller to allow access for all users and enhance user interface definitions

--- frontend/src/types/login.interface.ts
+++ frontend/src/types/login.interface.ts
@@ -3,3 +3,18 @@ export interface ILogin {
 	email: string
 	password: string
 }
+export interface IUser {
+	id: string
+	name: string
+	email: string
+	role: 'ADMIN' | 'WAITER' | 'COOK'
+	avatarUrl: string
+	createdAt: string
+	updatedAt: string
+}
+export interface IAuthContext {
+	user: IUser | null
+	isAuth: boolean
+	login: (data: ILogin) => Promise<void>
+	logout: () => void
+}
