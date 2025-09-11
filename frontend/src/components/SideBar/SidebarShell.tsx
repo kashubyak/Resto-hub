573208fa9fdd1354f40e27dedecd107903228822
kashubyak Mykola <kashubyak514@gmail.com>
2025-09-11 19:57:22 +0300

    refactor: remove props from Header component and utilize Zustand for sidebar state management

--- frontend/src/components/SideBar/SidebarShell.tsx
+++ frontend/src/components/SideBar/SidebarShell.tsx
@@ -1,20 +1,17 @@
 'use client'
 
 import { Header } from '@/components/container/Header'
-import { useSidebarStore } from '@/store/sidebar.store'
 import React from 'react'
 import { Sidebar } from './Sidebar'
 
 type Props = { children: React.ReactNode }
 
 export const SidebarShell = ({ children }: Props) => {
-	const { setMobileOpen, mobileOpen } = useSidebarStore()
-
 	return (
 		<div className='flex h-screen'>
 			<Sidebar />
 			<div className='flex-1 flex flex-col'>
-				<Header onOpenSidebar={() => setMobileOpen(true)} isSidebarOpen={mobileOpen} />
+				<Header />
 				<main className='flex-1 overflow-y-auto p-6'>{children}</main>
 			</div>
 		</div>
--- frontend/src/components/container/Header.tsx
+++ frontend/src/components/container/Header.tsx
@@ -1,24 +1,21 @@
 'use client'
 
 import { useUserRoutes } from '@/hooks/useUserRoutes'
+import { useSidebarStore } from '@/store/sidebar.store'
 import { cn } from '@/utils/cn'
 import MenuIcon from '@mui/icons-material/Menu'
 
-interface IHeaderProps {
-	onOpenSidebar: () => void
-	isSidebarOpen: boolean
-}
-
-export const Header = ({ onOpenSidebar, isSidebarOpen }: IHeaderProps) => {
+export const Header = () => {
 	const { currentRoute } = useUserRoutes()
+	const { setMobileOpen, mobileOpen } = useSidebarStore()
 
 	return (
 		<header className='h-[3.685rem] border-b border-border flex items-center justify-between px-4 bg-background md:hidden'>
 			<button
-				onClick={onOpenSidebar}
+				onClick={() => setMobileOpen(true)}
 				className={cn(
 					'p-2 rounded-lg hover:bg-secondary transition-colors duration-200',
-					isSidebarOpen ? 'opacity-0 pointer-events-none' : 'opacity-100',
+					mobileOpen ? 'opacity-0 pointer-events-none' : 'opacity-100',
 				)}
 				aria-label='Open menu'
 			>
