5137a2ffb8b27a7e1a459883c9273ae87f74861f
kashubyak Mykola <kashubyak514@gmail.com>
2025-09-13 16:42:54 +0300

    refactor: add className prop to AuthButton component for improved styling flexibility

--- frontend/src/components/ui/AuthButton.tsx
+++ frontend/src/components/ui/AuthButton.tsx
@@ -1,16 +1,23 @@
 'use client'
 
+import { cn } from '@/utils/cn'
+
 type AuthButtonProps = {
 	type: 'submit' | 'button' | 'reset'
 	onClick?: () => void
 	text: string
+	className?: string
 }
-export const AuthButton = ({ type, text, onClick }: AuthButtonProps) => {
+
+export const AuthButton = ({ type, text, onClick, className }: AuthButtonProps) => {
 	return (
 		<button
 			type={type}
 			onClick={onClick}
-			className='w-full bg-primary text-primary-foreground hover:bg-primary mt-2 py-2 px-4 rounded-md font-semibold'
+			className={cn(
+				'w-full bg-primary text-primary-foreground hover:bg-primary mt-2 py-2 px-4 rounded-md font-semibold',
+				className,
+			)}
 		>
 			{text}
 		</button>
