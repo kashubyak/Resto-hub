e8d6c9856c93973b7bde94c2eed14815edd362e5
kashubyak Mykola <kashubyak514@gmail.com>
2025-09-20 14:13:07 +0300

    feat: create useDishes hook and refactor DishesPage layout for improved structure

--- frontend/src/app/dish/page.tsx
+++ frontend/src/app/dish/page.tsx
@@ -3,20 +3,24 @@
 import { Button } from '@/components/ui/Button'
 import { useState } from 'react'
 import { DishModal } from './DishModal'
+import { DishList } from './components/DishList'
 
 export default function DishesPage() {
 	const [isModalOpen, setIsModalOpen] = useState(false)
 
 	return (
-		<div className='flex justify-between items-center p-4 border-b'>
-			<Button
-				type='button'
-				text='Create new dish'
-				onClick={() => setIsModalOpen(true)}
-				className='w-auto px-6 py-2'
-			/>
+		<div>
+			<div className='flex justify-between items-center p-4 border-b'>
+				<Button
+					type='button'
+					text='Create new dish'
+					onClick={() => setIsModalOpen(true)}
+					className='w-auto px-6 py-2'
+				/>
+				<DishModal open={isModalOpen} onClose={() => setIsModalOpen(false)} />
+			</div>
 
-			<DishModal open={isModalOpen} onClose={() => setIsModalOpen(false)} />
+			<DishList />
 		</div>
 	)
 }
