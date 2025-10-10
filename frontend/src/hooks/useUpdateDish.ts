e70d3a1be306fc2c85a743eeed9cbb7be4978b9b
kashubyak Mykola <kashubyak514@gmail.com>
2025-10-10 15:28:44 +0300

    feat: implement useUpdateDish hook for dish update functionality with form handling

---
+++ frontend/src/hooks/useUpdateDish.ts
@@ -0,0 +1,63 @@
+import { useAlert } from '@/providers/AlertContext'
+import type { IDish, IDishFormValues } from '@/types/dish.interface'
+import type { IAxiosError } from '@/types/error.interface'
+import { parseBackendError } from '@/utils/errorHandler'
+import { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import { useDishes } from './useDishes'
+
+export const useUpdateDish = (dishData: IDish | undefined, onClose: () => void) => {
+	const { showError, showSuccess } = useAlert()
+	const { refetchDishes } = useDishes()
+	const {
+		handleSubmit,
+		formState: { errors, isDirty },
+		reset,
+		control,
+		setError,
+		clearErrors,
+		watch,
+	} = useForm<IDishFormValues>({
+		mode: 'onChange',
+		defaultValues: dishData
+			? {
+					name: dishData.name,
+					description: dishData.description,
+					price: dishData.price,
+					categoryId: dishData.categoryId || undefined,
+					ingredients: dishData.ingredients,
+					weightGr: dishData.weightGr || undefined,
+					calories: dishData.calories || undefined,
+					available: dishData.available,
+			  }
+			: {
+					ingredients: [],
+					available: true,
+			  },
+	})
+
+	const onSubmit = useCallback(
+		async (data: IDishFormValues) => {
+			try {
+				console.log(data)
+				showSuccess('Dish updated successfully')
+				refetchDishes()
+				onClose()
+			} catch (err) {
+				showError(parseBackendError(err as IAxiosError).join('\n'))
+			}
+		},
+		[showSuccess, showError, refetchDishes, onClose],
+	)
+	return {
+		onSubmit,
+		errors,
+		handleSubmit,
+		control,
+		setError,
+		clearErrors,
+		watch,
+		isDirty,
+		reset,
+	}
+}
