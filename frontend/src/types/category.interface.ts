2006982ea732c073d676a19fbdd19243778aa525
kashubyak Mykola <kashubyak514@gmail.com>
2025-10-19 22:14:13 +0300

    feat: implement getCategoriesService to fetch categories with optional parameters

--- frontend/src/types/category.interface.ts
+++ frontend/src/types/category.interface.ts
@@ -1,6 +1,24 @@
+import type { IPaginatedResponse } from './api.interface'
+import type { IDish } from './dish.interface'
+
 export interface ICategory {
 	id: number
 	name: string
 	createdAt: string
 	updatedAt: string
+}
+
+export interface ICategoryWithDishes extends ICategory {
+	dishes: IDish[]
+}
+
+export type ICategoryListResponse = IPaginatedResponse<ICategoryWithDishes>
+
+export interface IGetAllCategoriesParams {
+	page?: number
+	limit?: number
+	search?: string
+	hasDishes?: boolean
+	sortBy?: string
+	order?: 'asc' | 'desc'
 }
