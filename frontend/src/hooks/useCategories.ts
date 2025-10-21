fd25a700ed33a0ebbb76d2c4a971a01b63aa52e4
kashubyak Mykola <kashubyak514@gmail.com>
2025-10-21 22:21:23 +0300

    feat: refactor useCategories hook to include allCategories and refetchCategories

--- frontend/src/hooks/useCategories.ts
+++ frontend/src/hooks/useCategories.ts
@@ -1,14 +1,17 @@
 'use client'
 
 import { CATEGORIES_QUERY_KEY, LIMIT } from '@/constants/query-keys.constant'
-import { useAlert } from '@/providers/AlertContext'
 import { getCategoriesService } from '@/services/category/get-categories.service'
-import type { ICategoryListResponse } from '@/types/category.interface'
+import type {
+	ICategoryListResponse,
+	ICategoryWithDishes,
+} from '@/types/category.interface'
 import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query'
+import { useCallback, useMemo } from 'react'
 
 export const useCategories = (searchQuery?: string) => {
 	const queryClient = useQueryClient()
-	const { showSuccess, showError } = useAlert()
+	// const { showSuccess, showError } = useAlert()
 	const normalizedSearchQuery = searchQuery?.trim() || undefined
 
 	const categoriesQuery = useInfiniteQuery<ICategoryListResponse, Error>({
@@ -28,5 +31,18 @@ export const useCategories = (searchQuery?: string) => {
 		refetchOnWindowFocus: false,
 	})
 
-	return {}
+	const refetchCategories = useCallback(() => {
+		queryClient.invalidateQueries({ queryKey: [CATEGORIES_QUERY_KEY.ALL] })
+	}, [queryClient])
+
+	const allCategories = useMemo<ICategoryWithDishes[]>(
+		() => categoriesQuery.data?.pages.flatMap(page => page.data) ?? [],
+		[categoriesQuery.data?.pages],
+	)
+
+	return {
+		...categoriesQuery,
+		allCategories,
+		refetchCategories,
+	}
 }
