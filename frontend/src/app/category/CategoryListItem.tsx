cdb481e6bbe945a3a1c7d04d9d918d120ad4e3de
kashubyak Mykola <kashubyak514@gmail.com>
2025-10-21 22:40:14 +0300

    feat: enhance CategoryListItem component with infinite scrolling and category fetching

--- frontend/src/app/category/CategoryListItem.tsx
+++ frontend/src/app/category/CategoryListItem.tsx
@@ -1,6 +1,36 @@
-import { memo } from 'react'
+import { useCategories } from '@/hooks/useCategories'
+import { memo, useCallback, useEffect, useRef } from 'react'
 
 const CategoryListItemComponent = () => {
+	const {
+		allCategories,
+		fetchNextPage,
+		hasNextPage,
+		isFetchingNextPage,
+		isLoading,
+		isError,
+	} = useCategories()
+	const loaderRef = useRef<HTMLDivElement | null>(null)
+
+	useEffect(() => {
+		console.log(allCategories)
+	}, [allCategories])
+
+	const handleIntersection = useCallback(
+		(entries: IntersectionObserverEntry[]) => {
+			if (entries[0].isIntersecting && hasNextPage) fetchNextPage()
+		},
+		[hasNextPage, fetchNextPage],
+	)
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(handleIntersection, {
+			threshold: 1.0,
+		})
+		if (loaderRef.current) observer.observe(loaderRef.current)
+		return () => observer.disconnect()
+	}, [handleIntersection])
+
 	return <div>Category List Item</div>
 }
 export const CategoryListItem = memo(CategoryListItemComponent)
