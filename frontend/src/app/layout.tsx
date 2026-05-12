f1c84611a02fdc4083524423f791565144e2fff5
Kashubyak Mykola <kashubyak514@gmail.com>
2026-05-12 21:33:51 +0300

    feat: enhance Next.js configuration and layout for improved functionality
    - Added Unsplash image domain to Next.js configuration for optimized image loading.
    - Updated RootLayout component to suppress hydration warnings, improving rendering stability.
    - Expanded ROLE_ROUTES_MAP to include waiter and cook order routes for better role-based access management.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body className={`${geistSans.variable} antialiased`}>
 				<Providers>{children}</Providers>
 			</body>
